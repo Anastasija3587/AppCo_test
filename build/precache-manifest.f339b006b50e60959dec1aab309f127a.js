e4606d7c627793fd955317597f401452b8d01514
Anastasija3587 <anastasija3587@gmail.com>
2020-06-20 15:23:49 +0300

    added build

---
+++ build/precache-manifest.f339b006b50e60959dec1aab309f127a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a230f0e365f338b28068dbea747920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1169e3e4cfeec8f2054",
+    "url": "/static/css/main.e3b8e396.chunk.css"
+  },
+  {
+    "revision": "4b36e2da8147f800e5b8",
+    "url": "/static/js/2.217da04f.chunk.js"
+  },
+  {
+    "revision": "44bb13c376a360a26522c8368cf779a1",
+    "url": "/static/js/2.217da04f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1169e3e4cfeec8f2054",
+    "url": "/static/js/main.52d03273.chunk.js"
+  },
+  {
+    "revision": "23994b8a0de37c42bde5",
+    "url": "/static/js/runtime-main.96ce6a62.js"
+  },
+  {
+    "revision": "be25a26c07bdac07a1b75717e12c27f1",
+    "url": "/static/media/backFooter.be25a26c.svg"
+  },
+  {
+    "revision": "5c8bf8cfce250bd48b69fec3146023e9",
+    "url": "/static/media/backSmall.5c8bf8cf.svg"
+  },
+  {
+    "revision": "105ae180f18cca9bf8b98cb6fd6df3c3",
+    "url": "/static/media/background.105ae180.svg"
+  },
+  {
+    "revision": "8c4a1a136c45cf2085bac0505a10278c",
+    "url": "/static/media/iphone.8c4a1a13.svg"
+  },
+  {
+    "revision": "a94fdcf32fb84c5eb9cd2e1a8e55920b",
+    "url": "/static/media/svgDesign.a94fdcf3.svg"
+  },
+  {
+    "revision": "5a6de470593a0c51e2ad0e489083a1e6",
+    "url": "/static/media/svgRetina.5a6de470.svg"
+  },
+  {
+    "revision": "75eee95fe905acfb9b0aca6e20854cdf",
+    "url": "/static/media/svgSecure.75eee95f.svg"
+  }
+]);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const corsMiddleware = require('cors');
+const path = require('path');
 const db = require('./models/index');
 const usersRouters = require('./app/user');
 
@@ -10,12 +11,19 @@ function start() {
   app.use(express.json());
   app.use(corsMiddleware());
 
+  app.use(express.static('build'));
+
   // Add all routers
   app.use('/api/users', usersRouters);
 
   // If route doesn't find
-  app.use('*', (req, res) => {
+  app.use('/api/*', (req, res) => {
     res.status(404).json('Error 404!');
+  });
+
+  // Handles any requests that don't match the ones above
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/../build/index.html`));
   });
 
   db.sequelize.sync().then(() => {
